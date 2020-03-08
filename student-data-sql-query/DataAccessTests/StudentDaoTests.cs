039254c408217a7b429b6d8d1387b4b77534b92c
nathalia-italiano <nathalia.italiano@atmira.com>
2020-03-08 13:06:29 +0100

    initial commit close #6 project with three tiers architecture

---
+++ student-data-sql-query/DataAccessTests/StudentDaoTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Tests
+{
+    [TestClass()]
+    public class StudentDaoTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ student-data-sql-query/README.md
@@ -0,0 +1,1 @@
+# Student Data via Sql Query
