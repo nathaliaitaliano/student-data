8be5efb966ec6bed2564d7c3966b06d0f8e07887
nathalia-italiano <nathalia.italiano@atmira.com>
2020-03-08 15:51:22 +0100

    SQL database connection close #11 through a method in data access

--- student-data-sql-query/DataAccess/StudentDao.cs
+++ student-data-sql-query/DataAccess/StudentDao.cs
@@ -1,6 +1,7 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,18 @@ namespace DataAccess
 {
     public class StudentDao : IStudentDao
     {
+        public void ConnectionDatabase()
+        {
+            string connectionConfiguration;
+            SqlConnection connection;
 
+            connectionConfiguration = @"Data Source=Localhost;Initial Catalog=Vueling;User ID=sa;Password=yourStrong(!)Password";
+            connection = new SqlConnection(connectionConfiguration);
+
+            connection.Open();
+            connection.Close();
+        }
+
+        public void Add(Student student);
     }
 }
