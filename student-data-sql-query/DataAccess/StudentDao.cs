1f1aff427bb3ab62b08ab8314b0a0be1a1be2b05
nathalia-italiano <nathalia.italiano@atmira.com>
2020-03-10 19:52:07 +0100

    logInfo initial for the methods in StudentDao class

--- student-data-sql-query/DataAccess/StudentDao.cs
+++ student-data-sql-query/DataAccess/StudentDao.cs
@@ -11,13 +11,15 @@ namespace DataAccess
 
         public void Add(Student student)
         {
+            log.Info($"ADD {student}");
             string sql = $"Insert into dbo.Students (Name, Surname, Birthday) " +
-                $"values('{student.Name}', '{student.Surname}', '{student.DateOfBirth}')";
-            Database.ExecuteQuery(sql);
+            $"values('{student.Name}', '{student.Surname}', '{student.DateOfBirth}')";
+                Database.ExecuteQuery(sql);
         }
 
         public List<Student> GetAllStudents()
         {
+            log.Info("SHOW all students list");
             string sql = "Select StudentId, Name, Surname, Birthday from dbo.Students";
             List<Student> studentsList = CreateStudentList(sql);
             return studentsList;
@@ -25,6 +27,7 @@ namespace DataAccess
 
         public Student GetStudentById(Int32 studentId)
         {
+            log.Info($"SEARCH a student with ID: {studentId}'");
             string sql = $"Select * from dbo.Students where StudentId='{studentId}'";
             List<Student> studentById = CreateStudentList(sql);
             return studentById[0];
@@ -32,6 +35,7 @@ namespace DataAccess
 
         public void UpdateStudent(Student student)
         {
+            log.Info($"UPDATE {student}'");
             string sql = $"Update dbo.Students set Name='{student.Name}', Surname='{student.Surname}', " +
                 $"Birthday='{student.DateOfBirth}' where StudentId='{student.StudentId}'";
             Database.ExecuteQuery(sql);
@@ -39,6 +43,7 @@ namespace DataAccess
 
         public void DeleteStudentById(Int32 studentId)
         {
+            log.Info($"DELETE a student with ID: {studentId}'");
             string sql = $"Delete dbo.Students where StudentId='{studentId}'";
             Database.ExecuteQuery(sql);
         }
